1db32ccda297280f9bef97379e49cfca6a12fa00
zyouime <igorneizvestny69@gmail.com>
2024-07-01 04:42:42 +0300

    zyouime

--- src/main/java/me/zyouime/lairytrapka/mixin/EntityRenderDispatcherMixin.java
+++ src/main/java/me/zyouime/lairytrapka/mixin/EntityRenderDispatcherMixin.java
@@ -1,13 +1,11 @@
 package me.zyouime.lairytrapka.mixin;
 
 import me.zyouime.lairytrapka.LairyTrapka;
-import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.EntityRenderDispatcher;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.ItemEntity;
-import net.minecraft.world.WorldView;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -16,17 +14,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(EntityRenderDispatcher.class)
 public class EntityRenderDispatcherMixin {
 
-    @Inject(method = "renderShadow", at = @At("HEAD"), cancellable = true)
-    private static void render(MatrixStack matrices, VertexConsumerProvider vertexConsumers, Entity entity, float opacity, float tickDelta, WorldView world, float radius, CallbackInfo ci) {
-        if (LairyTrapka.itemRenderer) {
-            if (entity instanceof ItemEntity) {
-                ci.cancel();
-            }
-        }
-    }
-
-    @Inject(method = "renderHitbox", at = @At("HEAD"), cancellable = true)
-    private void render(MatrixStack matrices, VertexConsumer vertices, Entity entity, float tickDelta, CallbackInfo ci) {
+    @Inject(method = "render", at = @At("HEAD"), cancellable = true)
+    private void render(Entity entity, double x, double y, double z, float yaw, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, CallbackInfo ci) {
         if (LairyTrapka.itemRenderer) {
             if (entity instanceof ItemEntity) {
                 ci.cancel();
